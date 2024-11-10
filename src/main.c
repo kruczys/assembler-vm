d0e9860f4ea98ac38ff8d2d82040fbd25c9a9893
Konrad Kreczko <konradkreczko@gmail.com>
2024-11-11 00:07:46 +0100

    feat: Added basic InstructionSet enum and program array

--- src/main.c
+++ src/main.c
@@ -1,3 +1,22 @@
+#include <stdlib.h>
+
+typedef enum {
+    MOV,
+    PSH,
+    ADD,
+    POP,
+    SET,
+    HLT
+} InstructionSet;
+
+const int program[] = {
+    PSH, 5,
+    PSH, 6,
+    ADD,
+    POP,
+    HLT
+};
+
 int main() {
-    return 1;
+    exit(EXIT_SUCCESS);
 }
